59b79ca5ee0f61e6503159d1b9c0808aad46da8d
thiagomsr20 <thiagomsr20@gmail.com>
2025-02-21 15:28:42 -0300

    Not done yet, just att files

---
+++ ExampleMod/Behavior/NoEnergyCostToSmithing.cs
@@ -0,0 +1,45 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.Core;
+
+namespace ExampleMod.Behavior
+{
+    public class NoEnergyCostToSmithing : DefaultSmithingModel
+    {
+        public override int GetEnergyCostForRefining(ref Crafting.RefiningFormula refineFormula, Hero hero)
+        {
+            ExplainedNumber explainedNumber = new ExplainedNumber(6f);
+
+            if (hero.GetPerkValue(DefaultPerks.Crafting.PracticalRefiner))
+            {
+                explainedNumber.AddFactor(DefaultPerks.Crafting.PracticalRefiner.PrimaryBonus);
+            }
+
+            return (int)explainedNumber.ResultNumber;
+        }
+
+        public override int GetEnergyCostForSmithing(ItemObject item, Hero hero)
+        {
+            ExplainedNumber explainedNumber = new ExplainedNumber(10 + 5 * (int)item.Tier);
+            if (hero.GetPerkValue(DefaultPerks.Crafting.PracticalSmith))
+            {
+                explainedNumber.AddFactor(DefaultPerks.Crafting.PracticalSmith.PrimaryBonus);
+            }
+
+            return (int)explainedNumber.ResultNumber;
+        }
+
+        public override int GetEnergyCostForSmelting(ItemObject item, Hero hero)
+        {
+            ExplainedNumber explainedNumber = new ExplainedNumber(10f);
+            if (hero.GetPerkValue(DefaultPerks.Crafting.PracticalSmelter))
+            {
+                explainedNumber.AddFactor(DefaultPerks.Crafting.PracticalSmelter.PrimaryBonus);
+            }
+
+            return (int)explainedNumber.ResultNumber;
+        }
+
+    }
+}
--- ExampleMod/MySubModule.cs
+++ ExampleMod/MySubModule.cs
@@ -1,25 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using ExampleMod.Behavior;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterDevelopment;
+using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
-using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 
+// TODO: Alterar no XML e o nome da solução e projeto para o nome BetterSmithing quando subir o mod para produção
 namespace ExampleMod
 {
     class MySubModule : MBSubModuleBase
     {
         protected override void OnSubModuleLoad()
         {
-            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
-             new TextObject("Message", null),
-             9990,
-             () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
-             () => { return (false, null); }));
+            base.OnSubModuleLoad();
+        }
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+
+            if (!(game.GameType is Campaign))
+                return;
+
+            InformationManager.DisplayMessage(new InformationMessage("NoEnergyCostToSmithing carregado!"));
+            AddBehaviors(gameStarterObject as CampaignGameStarter);
+        }
+
+        private void AddBehaviors(CampaignGameStarter gameStarterObject)
+        {
+            if (gameStarterObject != null)
+            {
+                // Remove qualquer modelo existente de smithing antes de adicionar o nosso
+                gameStarterObject.RemoveModel<DefaultSmithingModel>();
+                gameStarterObject.AddModel(new NoEnergyCostToSmithing());
+                InformationManager.DisplayMessage(new InformationMessage("Modelo de Smithing modificado!"));
+            }
         }
     }
 }
