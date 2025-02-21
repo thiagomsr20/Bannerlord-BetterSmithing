7ec826bcf7620fcda3c454ce93b5b7ec31cb402d
thiagomsr20 <thiagomsr20@gmail.com>
2025-02-21 13:37:44 -0300

    Initial mod hello work files

---
+++ ExampleMod/MySubModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace ExampleMod
+{
+    class MySubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+             new TextObject("Message", null),
+             9990,
+             () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
+             () => { return (false, null); }));
+        }
+    }
+}
